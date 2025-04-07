14e26a6a57c4327c38e4e674bdd1c65491beded5
Mathias <math.kuni123@gmail.com>
2025-04-07 09:40:42 -0500

    feat(main): add main compilation

--- main.cpp
+++ main.cpp
@@ -7,6 +7,26 @@
 int main() {
     try {
         const auto compilationProcess = std::unique_ptr<Process>(
-            new Process())
+            new Process(
+                "CompileMain",
+                "Compile main.cpp to main.exe",
+                { "CentralProcessingUnit", "Memory"},
+                15));
+        //Add resource to the process
+        compilationProcess->addResource(
+            std::unique_ptr<ConsumableResource>(
+                new ConsumableResource("Memory", 4096)));
+        compilationProcess->addResource(
+            std::unique_ptr<UsableResource>(
+                new UsableResource("CentralProcessingUnit", 32)));
+        compilationProcess->addTask(
+            std::unique_ptr<Task>(
+                new Task("ScanSourceCode", "Tokenize main.c",{"CentralProcessingUnit", "Memory"}, 2)));
+        std::cout<<"Running process: "<<compilationProcess->getName()<<std::endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "Critical error: " << e.what() << std::endl;
+        return 1;
     }
+    return 0;
 }
